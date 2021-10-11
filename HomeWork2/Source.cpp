4156c0f0386e300aa05f24fc9af5ec85ff4369ce
dontlover2329r23 <91621645+dontlover2329r23@users.noreply.github.com>
2021-10-11 19:24:16 +0300

    Update Source.cpp

--- HomeWork2/Source.cpp
+++ HomeWork2/Source.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-int del1(int a){
+int del1(int a) {
 	int i;
 	int sum = 0;
-	for (i = 0; i < a; i++) {
+	for (i = 1; i < a; i++) {
 		if (a % i == 0)
 			sum += i;
 	}
@@ -12,7 +12,7 @@ int del1(int a){
 int del2(int b) {
 	int i;
 	int sum = 0;
-	for (i = 0; i < b; i++) {
+	for (i = 1; i < b; i++) {
 		if (b % i == 0)
 			sum += i;
 	}
@@ -20,14 +20,21 @@ int del2(int b) {
 }
 int main() {
 	setlocale(LC_ALL, "ru");
-	int x, y, a, b;
-	cout << "Enter two numbers";
-	cin >> a >> b;
-	x = del1(a);
-	y = del2(b);
-	if ((a = y) && (b = x)) {
-		cout << "These are friendly numbers!";
+	int x, y, a, b, i, j;
+	j = 0;
+	for (i = 1; i <= 10000; i++) {
+		    a = i;
+			x = del1(a);
+			b = x;
+			y = del2(b);
+			if (a!=j) {
+				if ((a == y) && (a != x)) {
+					j = b;
+					cout << a << " " << b << endl;
+				}
+			}
+			x = 0;
+			y = 0;
+		
 	}
-	else
-		cout << "These are not friendly numbers!";
 }
