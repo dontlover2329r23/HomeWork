da8925312400f927994e489927d6b3da393d2efc
dontlover2329r23 <91621645+dontlover2329r23@users.noreply.github.com>
2024-05-04 16:26:35 +0300

    Update 3.py

--- 2024MS/3zadanie/3.py
+++ 2024MS/3zadanie/3.py
@@ -2,9 +2,7 @@ from scipy import stats
 from scipy.optimize import minimize
 from scipy import stats
 # Данные
-data = ['B', 'B', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'A', 'B', 'A', 'A', 'A', 'B', 'A', 'B', 'B', 'A', 'B',
-        'A', 'B', 'B', 'A', 'B', 'B', 'B', 'A', 'B', 'B', 'B', 'B', 'B', 'A', 'B', 'B', 'B', 'B', 'B', 'B',
-        'B', 'A', 'B', 'B', 'A', 'A', 'A', 'B', 'B', 'B', 'A', 'B', 'B']
+data = ['B'	,'B',	'A',	'B',	'B',	'B',	'B',	'B',	'B',	'A',	'B',	'A',	'A',	'A',	'B',	'A',	'B',	'B',	'A',	'B',	'A',	'B',	'B',	'A',	'B',	'B',	'B',	'A',	'B',	'B',	'B',	'B',	'B',	'B',	'A',	'B',	'B',	'B',	'B',	'B',	'B',	'B',	'B',	'B',	'B',	'A',	'B',	'B',	'A',	'A',	'A',	'B',	'B',	'B',	'A',	'B',	'B',	'A',	'A',	'B',	'B',	'B'        ]
 
 # Подсчет количества успешных исходов
 successes = data.count('A')
