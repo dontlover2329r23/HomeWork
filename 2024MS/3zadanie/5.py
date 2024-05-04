0508da2b4ea38f5717259fa9e3cc3335fb1708a9
dontlover2329r23 <91621645+dontlover2329r23@users.noreply.github.com>
2024-05-04 21:37:56 +0300

    Update 5.py

--- 2024MS/3zadanie/5.py
+++ 2024MS/3zadanie/5.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import chi2
 # Данные
 data_group1 = np.array([20.4, 13.1, 14.7, 17.3, 15.8, 18.3, 16.8, 16.8, 19.9, 17.3, 14, 18.4, 12.9, 16.3, 15.6, 18.5,
                         18.6, 14.1, 15.2, 13.6, 19.3, 15.8, 14.3, 18.6, 19.6, 15.4, 20.3, 14, 16.5, 17.8, 16.2, 17.9,
@@ -22,19 +22,20 @@ alpha = 0.025
 X0 = 13.7
 delta = 1
 r = 8
-interval_bounds = np.linspace(X0 - delta, X0 + delta, r+1)
+intervals = [X0 - 10] + [X0 + i * delta for i in range(r - 1)] + [X0 + r * delta + 10]
+print(f"{intervals} - интервалы")
 # Построение графиков
 plt.figure(figsize=(8, 10))
 
 plt.subplot(2, 1, 1)
-plt.hist(data_group1, bins=interval_bounds, color='b', alpha=0.7, label='Group 1', edgecolor='black')
+plt.hist(data_group1, bins=intervals, color='b', alpha=0.7, label='Group 1', edgecolor='black')
 plt.legend()
 plt.title('Frequency Distribution of Group 1')
 plt.xlabel('Intervals')
 plt.ylabel('Frequency')
 
 plt.subplot(2, 1, 2)
-plt.hist(data_group2, bins=interval_bounds, color='r', alpha=0.7, label='Group 2', edgecolor='black')
+plt.hist(data_group2, bins=intervals, color='r', alpha=0.7, label='Group 2', edgecolor='black')
 plt.legend()
 plt.title('Frequency Distribution of Group 2')
 plt.xlabel('Intervals')
@@ -42,26 +43,27 @@ plt.ylabel('Frequency')
 
 plt.tight_layout()
 plt.show()
-
-
+n1=len(data_group1)
+n2=len(data_group1)
 # Вычисление статистики Хи-квадрат
-observed_group1, _ = np.histogram(data_group1, bins=r)
-observed_group2, _ = np.histogram(data_group2, bins=r)
-observed_total = observed_group1 + observed_group2
+observed_group1, _ = np.histogram(data_group1, bins=intervals)
+observed_group2, _ = np.histogram(data_group2, bins=intervals)
+print(observed_group1)
+print(observed_group1 / n1)
+print(observed_group2)
+print(observed_group2 / n2)
 n1 = len(data_group1)
 n2 = len(data_group2)
-
-chi2_statistic = n1*n2*np.sum((observed_total - (observed_group1*n1/n1) - (observed_group2*n1/n2))**2 / (observed_total))
 degree_of_freedom = r - 1
-
-print(f"Хи-квадрат статистика: {chi2_statistic}")
-print(f"Степени свободы: {degree_of_freedom}")
-
-# Определение критического значения и p-value
-from scipy.stats import chi2
-
 critical_value = chi2.ppf(1 - alpha, degree_of_freedom)
-p_value = 1 - chi2.cdf(chi2_statistic, degree_of_freedom)
-
 print(f"Критическое значение: {critical_value}")
-print(f"P-value: {p_value}")
+#вычислеснение хи- квадрат
+XI2 = []
+for i in range(r):
+    XI2.append(
+        (observed_group1[i] / n1- observed_group2[i] / n2) ** 2 / (observed_group1[i] + observed_group2[i]) * (n1 * n2))
+print(XI2)
+print('XI2 =', np.sum(XI2))
+# Вычисление p-value
+p_value = chi2.pdf(np.sum(XI2), r - 1)
+print(f"Критический уровень значимости p-value: {p_value}")
